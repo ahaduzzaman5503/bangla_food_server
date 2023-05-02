f04caafe6a50f6885d4df6440afb3c73bc348d52
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2023-05-02 14:03:30 +0600

    cors policiy solved and dynamic route setup

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const app = express()
-const chefs = require('./chefs.json')
-const port = 5000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const chefs = require('./chefs.json');
+const port = 5000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello Are you Okey? Hello Are you Okey?')
-})
+});
+
 app.get('/chefs', (req, res) => {
   res.send(chefs)
+});
+
+app.get('/chefs/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const chef = chefs.find(chef => chef.id === id) || {};
+  res.send(chef)
 })
 
 app.listen(port, () => {
